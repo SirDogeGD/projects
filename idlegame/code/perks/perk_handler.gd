2cb5d447767a899a1fb0f4dc850fbc34fd9a0f90
SirDogeGD <54358176+SirDogeGD@users.noreply.github.com>
2022-05-22 22:56:18 +0200

    renaming p1
    starting to rename IDs from numbers to words

--- idlegame/code/perks/perk_handler.gd
+++ idlegame/code/perks/perk_handler.gd
@@ -2,51 +2,54 @@ extends Node
 
 var rng = RandomNumberGenerator.new()
 var combo = 0
+var perks_a
+var perks_b
 
 #weapon dmg perks/a = attacker/b = attacked person/d = dmg
 #weapon base dmg perks
 func offensive_one(a, b, d):
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
-	if 0 in perks_a:
-		d += 1
-	if 8 in perks_a:
-		d += 1
+	get_perks(a, b)
+	for p in perks_a:
+		match p:
+			"BARB":
+				d += 1
+			"DIA_SWORD":
+				d += 1
 	return d
 #weapon multi perks
 func offensive_two(a, b, d):
 	rng.randomize()
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
-	if 1 in perks_a:
-		d = d*1.04
-	if 2 in perks_a:
-		if(b.current_hp <= b.hp/2):
-			d = d*1.06
-	if 3 in perks_a:
-		if(b.current_hp >= b.hp/2):
-			d = d*1.07
-	if 4 in perks_a:
-		for n in range(a.hp - a.current_hp):
-			d = d*1.01
-	if 5 in perks_a:
-		if(a.current_shield > 0):
-			d = d*1.05
-	if 9 in perks_a:
-		if rng.randi_range(1, 100) <= 12:
-			d = d*1.5
-	if 11 in perks_a:
-		if combo%4 == 0:
-			d = d*1.2
-	if 16 in perks_a:
-		if a.first_strike:
-			d = d*1.35
-			a.first_strike = false
+	get_perks(a, b)
+	for p in perks_a:
+		match p:
+			"SHARP":
+				d = d*1.04
+			"PUN":
+				if(b.current_hp <= b.hp/2):
+					d = d*1.06
+			"K_BUST":
+				if(b.current_hp >= b.hp/2):
+					d = d*1.07
+			"PF":
+				for n in range(a.hp - a.current_hp):
+					d = d*1.01
+			"G_A_B":
+				if(a.current_shield > 0):
+					d = d*1.05
+			"BERS":
+				if rng.randi_range(1, 100) <= 12:
+					d = d*1.5
+			"C_DMG":
+				if combo%4 == 0:
+					d = d*1.2
+			"F_STRIKE":
+				if a.first_strike:
+					d = d*1.35
+					a.first_strike = false
 	return d
 #weapon true perks
 func offensive_three(a, b, d):
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
+	get_perks(a, b)
 	if 12 in perks_a:
 		if combo%4 == 0:
 			a.heal(0, 0.8)
@@ -59,8 +62,7 @@ func offensive_three(a, b, d):
 	return d
 #armor base perks
 func defensive_one(a, b, armor):
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
+	get_perks(a, b)
 	if 6 in perks_b:
 		armor += 5
 	if 7 in perks_b:
@@ -68,17 +70,19 @@ func defensive_one(a, b, armor):
 	return armor
 #armor multi perks
 func defensive_two(a, b, armor):
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
+	get_perks(a, b)
 	return armor
 #armor true perks
 func defensive_three(a, b, d):
-	var perks_a = a.get_perks()
-	var perks_b = b.get_perks()
+	get_perks(a, b)
 	return d
 
 func on_kill():
 	var perks = you.get_perks()
-	if 13 in perks:
+	if "C_JAN" in perks:
 		var e = effect_handler.new_effect("res", 1, 2)
 		effect_handler.add_effect(e, you)
+
+func get_perks(a, b):
+	perks_a = a.get_perks()
+	perks_b = b.get_perks()
--- idlegame/code/perks/perk_info.gd
+++ idlegame/code/perks/perk_info.gd
@@ -5,41 +5,41 @@ var choice = [1, 2, 3]
 
 func perkinfo(id):
 	match id:
-		0:
+		0, "BARB":
 			return make("Barbarian", "Your weapon deals +1 dmg")
-		1:
+		1, "SHARP":
 			return make("Sharp", "Deal +4% melee damage")
-		2:
+		2, "PUN":
 			return make("Punisher", "Deal +6% damage vs. players below 50% HP")
-		3:
+		3, "K_BUST":
 			return make("King Buster", "Deal +7% damage vs. players above 50% HP")
-		4:
+		4, "PF":
 			return make("Pain Focus", "Deal +1% damage per hp❤ you're missing")
-		5:
+		5, "G_A_B":
 			return make("Gold and Boosted", "Deal +5% damage when you have shield hp")
-		6:
+		6, "DIA_BOOT":
 			return make("Diamond boots", "Gain +5 armor")
-		7:
+		7, "DIA_CHEST":
 			return make("Diamond Chestplate", "Gain +6 armor")
-		8:
+		8, "DIA_SWORD":
 			return make("Diamond Sword", "Your weapon deals +1 dmg")
 #			mysticism 1
-		9:
+		9, "BERS":
 			return make("Berserker", "12% chance to crit for 50% extra damage")
-		10:
+		10, "GUTS":
 			return make("Guts", "Heal 0.25hp on kill")
-		11:
+		11, "C_DMG":
 			return make("Combo: Damage", "Every fourth strike deals +20% damage")
-		12:
+		12, "C_SHIELD":
 			return make("Combo: Shield", "Every fourth strike gives 0.8 shield hp")
 #		mysticism 2
-		13:
+		13, "C_JAN":
 			return make("Counter-Janitor", "Gain Resistance I (2 turns) on kill")
-		14:
+		14, "C_CRUSH":
 			return make("Combo: Crush", "Every fourth strike apply Weakness I for a turn")
-		15:
+		15, "LS":
 			return make("Lifesteal", "Heal for 4% of damage dealt")
-		16:
+		16, "F_STRIKE":
 			return make("First Strike", "First hit against an enemy deals +35% dmg")
 
 func make(n, d):
@@ -50,9 +50,9 @@ func make(n, d):
 
 func make_choice():
 	var array_pool = []
-	var default_perks = [1, 2, 3, 4, 5]
-	var myst1 = [9, 10, 11, 12]
-	var myst2 = [13, 14, 15, 16]
+	var default_perks = ["SHARP", "PUN", "K_BUST", "PF", "G_A_B"]
+	var myst1 = ["BERS", "GUTS", "C_DMG", "C_SHIELD"]
+	var myst2 = ["C_JAN", "C_CRUSH", "LS", "F_STRIKE"]
 #	add perks to pool
 	array_pool += default_perks
 	if 1 in stats.pUpgrades:
