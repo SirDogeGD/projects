cfe7e35ba747dace7656117884db0180d0e488e7
Max <max.stoll@students.ksba.ch>
2022-05-29 23:57:15 +0200

    heartbar
    heartbar almost done, just need to figure out how to make it overlap stuff instead of moving everything around

--- idlegame/code/UI/hp/HeartBar.gd
+++ idlegame/code/UI/hp/HeartBar.gd
@@ -10,22 +10,22 @@ var shield_full = preload("res://icons/ui/hp/hud_shieldHeart.png")
 
 func update_health(value, max_hp, shield):
 	
+#	remove all children
 	for n in get_children():
-		if n.name != "1": 
-			remove_child(n)
-			n.queue_free()
+		remove_child(n)
+		n.queue_free()
 	
+#	add hearts
 	for i in max_hp/2:
-		add_child(TextureRect.new())
 		if value > i * 2 + 1:
 			add_heart(heart_full)
 		elif value > i * 2:
 			add_heart(heart_half)
 		else:
 			add_heart(heart_empty)
-
+	
+#	add shield hearts
 	for i in shield/2:
-		add_child(TextureRect.new())
 		if value > i * 2 + 1:
 			add_heart(shield_full)
 		elif value > i * 2:
@@ -34,18 +34,32 @@ func update_health(value, max_hp, shield):
 func add_heart(t):
 	var tr = TextureRect.new()
 	tr.texture = t
+	tr.rect_scale = Vector2(0,0)
 	var placed = false
+	var grid : GridContainer
 	
+#	make sure there is at least one grid
+	if get_child_count() < 1:
+		add_child(new_grid())
+	
+	grid = get_child(0)
+	
+#	players hpbar needs this extra BS
 	if who == "you":
 		for e in get_children():
 			if e.get_child_count() < 10:
-				e.add_child(tr)
+				grid = e
 				placed = true
+#		if all grids are full add a new one
 		if not placed:
-			var grid = GridContainer.new()
-			grid.columns = 10
+			grid = new_grid()
 			add_child(grid)
-			grid.add_child(tr)
 			move_child(grid, 0)
-	else:
-		pass
+	
+	grid.add_child(tr)
+
+func new_grid():
+	var newgrid = GridContainer.new()
+	newgrid.columns = 10
+	newgrid.size_flags_vertical = SIZE_FILL
+	return newgrid
