d4f6b386f56aac9c8d2281835ed60e2c201ec1fd
SirDogeGD <54358176+SirDogeGD@users.noreply.github.com>
2023-03-12 22:10:42 +0100

    movement and dash
    added 8 directional movement,
    dash (could be improved)
    particles on dash (yay)

--- PitRouge/game/person/person.gd
+++ PitRouge/game/person/person.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 class_name person
 
+var SPEED := 300.0
+
 func _physics_process(delta):
-	move_and_slide()
+	pass
+
+func dash(direction : Vector2):
+	if $DashTime.is_stopped() and velocity.x + velocity.y != 0:
+		$DashTime.start()
+		$DashParticles.emitting = true
+		SPEED *= 6
+		velocity = direction * SPEED
+		await $DashTime.timeout
+		$DashParticles.emitting = false
+		SPEED /= 6
--- PitRouge/game/person/player/player.gd
+++ PitRouge/game/person/player/player.gd
@@ -1,14 +1,13 @@
 extends person
 class_name player
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
+	
 	var input_direction := Vector2(Input.get_action_strength("right") - Input.get_action_strength("left")
 	, Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
 	velocity = input_direction * SPEED
+	
+	if Input.is_action_just_pressed("space"):
+		dash(input_direction)
 	
 	move_and_slide()
