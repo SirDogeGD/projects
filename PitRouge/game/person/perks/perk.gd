5fb3572aec37d5303743f1da92ac2ff55b15cfee
SirDogeGD <maxderdachs5@gmail.com>
2026-02-16 16:59:29 +0100

    perks
    little more work on perk rework

--- PitRouge/game/person/perks/perk.gd
+++ PitRouge/game/person/perks/perk.gd
@@ -3,8 +3,18 @@ class_name perk
 
 @export var id : String
 @export var listen_for : String
+@export var pname : String
+@export var desc : String
+@export var nums : Array[Array]
+@export var pool : String
+@export var rare : bool
+@export var tags : Array[String]
+@export var unlock : String
 
-func handle_signal(signal_id, data):
+func register():
+	SIGNAL.listen.connect(handle_signal)
+
+func handle_signal(signal_id : String, data):
 	if signal_id == listen_for:
 		handle()
 
